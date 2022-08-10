7c01976d136a72b4c4c319e3daaa0f138c8e192e
AlexandreJareck <alexandrejareck@gmail.com>
2022-08-09 22:30:24 -0300

    feat: add global styles in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../styles/global.styles'
 import { theme } from '../styles/theme.styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
   )
